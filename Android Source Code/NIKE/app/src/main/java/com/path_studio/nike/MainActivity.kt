8e23f7d5ec15c57d6ba6e45768f6c9bf61e44d9a
Patricia Fiona <32255348+patriciafiona@users.noreply.github.com>
2021-12-08 16:50:01 +0700

    [Android] Init Android Project

---
+++ Android Source Code/NIKE/app/src/main/java/com/path_studio/nike/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.path_studio.nike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
