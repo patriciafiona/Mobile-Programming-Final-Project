d364fa5bb8af11e7bd443196c7c9df6ebf681821
Patricia Fiona <32255348+patriciafiona@users.noreply.github.com>
2021-12-11 10:04:25 +0700

    [Android] Add Missing Photos, Add Chip Listener & Logic, Show RecycleView By Selected Chip
    Next:
    - limit collection results
    - get product by category & type
    - add show all button

--- Android Source Code/NIKE/app/src/main/java/com/path_studio/nike/ui/main/home/HomeFragment.kt
+++ Android Source Code/NIKE/app/src/main/java/com/path_studio/nike/ui/main/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.path_studio.nike.ui.main.home
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,6 +15,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
+import androidx.core.view.iterator
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,13 +23,12 @@ import com.faltenreich.skeletonlayout.Skeleton
 import com.faltenreich.skeletonlayout.applySkeleton
 import com.google.android.material.chip.Chip
 import com.path_studio.moviecatalogue.vo.Status
-import com.path_studio.nike.R
-import com.path_studio.nike.databinding.FragmentFavoriteBinding
 import com.path_studio.nike.databinding.FragmentHomeBinding
 import com.path_studio.nike.ui.main.home.adapter.CollectionAdapter
 import com.path_studio.nike.viewModel.ViewModelFactory
 
-
+import com.google.android.material.chip.ChipDrawable
+import com.path_studio.nike.R
 
 
 class HomeFragment : Fragment() {
@@ -34,6 +36,7 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding as FragmentHomeBinding
 
+    private lateinit var viewModel:HomeViewModel
     private var selectedCategory = 1
 
     override fun onCreateView(
@@ -48,72 +51,19 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.categoriesChipsContainer.isSelectionRequired = true
+
         if (activity != null) {
             val factory = ViewModelFactory.getInstance(requireActivity())
-            val viewModel = ViewModelProvider(requireActivity(), factory)[HomeViewModel::class.java]
+            viewModel = ViewModelProvider(requireActivity(), factory)[HomeViewModel::class.java]
 
-            val movieAdapter = CollectionAdapter()
-            viewModel.getAllCategory().observe(requireActivity(), { categories ->
-                if (categories != null) {
-                    when (categories.status) {
-                        Status.LOADING -> {}
-                        Status.SUCCESS -> {
-                            val catData = categories.data
-                            if(catData != null && catData.size > 0){
-                                for (d in catData){
-                                    val chip = Chip(ContextThemeWrapper(activity, com.path_studio.nike.R.style.ThinnerChip))
-                                    chip.text = d.name
-                                    chip.isCloseIconVisible = false
-                                    if(d.id != selectedCategory.toLong()) {
-                                        chip.setChipBackgroundColorResource(com.path_studio.nike.R.color.white)
-                                        chip.setTextColor(resources.getColor(com.path_studio.nike.R.color.black, null))
-                                    }else{
-                                        chip.setChipBackgroundColorResource(com.path_studio.nike.R.color.black)
-                                        chip.setTextColor(resources.getColor(com.path_studio.nike.R.color.white, null))
-                                    }
-
-                                    chip.setOnClickListener {
-                                        //go to category result
-                                    }
-
-                                    binding.categoriesChipsContainer.addView(chip)
-                                }
-                            }
-                        }
-                        Status.ERROR -> {
-                            Toast.makeText(context, "Something Error", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-            })
-
-            viewModel.getProductsByCategory(1).observe(requireActivity(), { products ->
-                if (products != null) {
-                    when (products.status) {
-                        Status.LOADING -> binding.progressBarCollection.visibility = View.VISIBLE
-                        Status.SUCCESS -> {
-                            binding.progressBarCollection.visibility = View.GONE
-                            movieAdapter.submitList(products.data)
-                        }
-                        Status.ERROR -> {
-                            binding.progressBarCollection.visibility = View.GONE
-                            Toast.makeText(context, "Something Error", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-            })
-
-            with(binding) {
-                rvCollections.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
-                rvCollections.setHasFixedSize(true)
-                rvCollections.adapter = movieAdapter
-            }
+            prepareCategoryChips()
         }
 
         //scroll listener
         binding.scrollContainer.viewTreeObserver.addOnScrollChangedListener {
             val scrollY: Int = binding.scrollContainer.scrollY
-            val navView = requireActivity().findViewById<View>(R.id.top_nav_container)
+            val navView = requireActivity().findViewById<View>(com.path_studio.nike.R.id.top_nav_container)
 
             if (scrollY in 0..70) {
                 navView.visibility = View.VISIBLE
@@ -121,9 +71,93 @@ class HomeFragment : Fragment() {
                 navView.visibility = View.INVISIBLE
             }
         }
+
+        //chip group listener
+        binding.categoriesChipsContainer.setOnCheckedChangeListener { chipGroup, id ->
+            prepareCollectionRV(viewModel, chipGroup.checkedChipId)
+        }
     }
 
-    private fun showHideDataIndicator(status: String){
+    @SuppressLint("UseCompatLoadingForColorStateLists")
+    private fun prepareCategoryChips(){
+        viewModel.getAllCategory().observe(requireActivity(), { categories ->
+            if (categories != null) {
+                when (categories.status) {
+                    Status.LOADING -> {}
+                    Status.SUCCESS -> {
+                        val catData = categories.data
+                        if(catData != null && catData.size > 0){
+                            var isFirstCategory = true
+                            for (d in catData){
+                                val chip = Chip(requireActivity())
+                                val chipDrawable = ChipDrawable.createFromAttributes(
+                                    requireActivity(),
+                                    null,
+                                    0,
+                                    R.style.CustomChipChoice
+                                )
+                                chip.setChipDrawable(chipDrawable)
+                                chip.isClickable = true
+
+                                val chipId = d.id.toInt()
+                                chip.id = chipId
+                                chip.tag = chipId
+
+                                chip.text = d.name
+                                chip.setTextColor(resources.getColorStateList(R.color.text_color_chip_state_list))
+                                chip.isCloseIconVisible = false
+
+                                chip.setOnClickListener {
+                                    binding.categoriesChipsContainer.check(chipId)
+                                }
+
+                                binding.categoriesChipsContainer.addView(chip)
+
+                                if(isFirstCategory){
+                                    selectedCategory = chipId
+                                    binding.categoriesChipsContainer.check(chip.id)
+                                    isFirstCategory = false
+                                }
+                            }
+                        }
+                    }
+                    Status.ERROR -> {
+                        Toast.makeText(context, "Something Error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun prepareCollectionRV(viewModel: HomeViewModel, categoryId: Int){
+        val collectionAdapter = CollectionAdapter()
+        viewModel.getProductsByCategory(categoryId).observe(requireActivity(), { products ->
+            if (products != null) {
+                when (products.status) {
+                    Status.LOADING -> binding.progressBarCollection.visibility = View.VISIBLE
+                    Status.SUCCESS -> {
+                        showHideDataIndicator("collection", true)
+                        binding.progressBarCollection.visibility = View.GONE
+                        collectionAdapter.submitList(products.data)
+                    }
+                    Status.ERROR -> {
+                        binding.progressBarCollection.visibility = View.GONE
+                        Toast.makeText(context, "Something Error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                showHideDataIndicator("collection", false)
+            }
+        })
+
+        with(binding) {
+            rvCollections.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+            rvCollections.setHasFixedSize(true)
+            rvCollections.adapter = collectionAdapter
+        }
+    }
+
+    private fun showHideDataIndicator(status: String, hide: Boolean){
         with(binding){
             when(status){
                 "all" ->{
@@ -142,6 +176,25 @@ class HomeFragment : Fragment() {
                     noSneakerIcon.visibility = View.VISIBLE
                     noSneakerTxt.visibility = View.VISIBLE
                 }
+                "collection" ->{
+                    if (hide){
+                        noCollectionIcon.visibility = View.GONE
+                        noCollectionTxt.visibility = View.GONE
+                    }else{
+                        noCollectionIcon.visibility = View.VISIBLE
+                        noCollectionTxt.visibility = View.VISIBLE
+                    }
+                }
+                "latest" ->{
+                    if (hide){
+                        noLatestIcon.visibility = View.GONE
+                        noLatestTxt.visibility = View.GONE
+                    }else{
+                        noLatestIcon.visibility = View.VISIBLE
+                        noLatestTxt.visibility = View.VISIBLE
+                    }
+                }
+                else ->{}
             }
         }
     }
